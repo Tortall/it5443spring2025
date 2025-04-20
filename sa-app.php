692ae1efdef62f9b811bb64e74e468906460f484
Tortall <96546627+Tortall@users.noreply.github.com>
2025-04-20 16:23:50 -0400

    Added link breaks.

--- sa-app.php
+++ sa-app.php
@@ -22,13 +22,13 @@
         <form action="sa-app-submitted.php" method="post" id="caritAppForm">
     
             <label for="firstName">First Name:</label>
-            <input type="text" name="firstName" id="firstName" required>
+            <input type="text" name="firstName" id="firstName" required><br />
 
             <label for="lastName">Last Name:</label>
-            <input type="text" name="lastName" id="lastName" required>
+            <input type="text" name="lastName" id="lastName" required><br />
 
             <label for="email">Email:</label>
-            <input type="email" name="email" id="email" required>
+            <input type="email" name="email" id="email" required><br />
 
             <label>Student Status:</label>
             <div id="radioForm">
@@ -37,7 +37,7 @@
                 <br />
                 <input type="radio" name="status" id="grad" value="Graduate">
                 <label for="grad" style="display:inline;">Graduate</label>
-            </div>
+            </div><br />
 
             <label for="major">Major:</label>
             <select name="major" id="major" required>
@@ -48,15 +48,15 @@
                 <option value="Game Design and Development">Game Design and Development</option>
                 <option value="Data Science">Data Science</option>
                 <option value="Artificial Intelligence">Artificial Intelligence</option>
-            </select>
+            </select><br />
 
     <label for="reason">Why should you be selected as a Student Assistant for CARIT?</label>
-    <textarea name="reason" id="reason" required></textarea>
+    <textarea name="reason" id="reason" required></textarea><br />
 
     <div id="checkboxForm">
         <input type="checkbox" id="agreement" name="agreement">
         <label for="agreement" style="display:inline;">I understand that filling out this form does not guarantee me a position with CARIT</label>
-    </div>
+    </div><br />
 
     <div id="submitButton">
         <button type="submit" id="submitBtn" disabled>Submit</button>
