5e0b525b15bf91b8e5a8e35147939df62863320b
Tortall <96546627+Tortall@users.noreply.github.com>
2025-04-20 15:24:35 -0400

    Created header.php and changed all extensions to php

---
+++ header.php
@@ -0,0 +1,11 @@
+<div id="container">
+        <a href="https://www.kennesaw.edu/ccse/academics/information-technology/index.php">
+        <img src ="/docs/ccse_ksu_logo.png" alt="CCSE KSU Logo" width="100" style="display: inline;"></a>
+        <img src="/docs/carit-logo.png" alt="Center for Applied Research in Information Technology" height="100" style="display:inline;">
+    </div>
+	<div id="header">Menu:
+	    <a href="index.php">Home</a> | 
+        <a href="about.php">About Us</a> | 
+	    <a href="schedule.php">Schedule</a> |
+        <a href="student-assistant.php">Summer Student Assistants</a>
+    </div>
--- student-assistant.php
+++ student-assistant.php
@@ -95,17 +95,8 @@ console.log(typeof grade1)
     </script>
 </head>
 <body>
-	<div id="container">
-        <a href="https://www.kennesaw.edu/ccse/academics/information-technology/index.php">
-		<img src ="/docs/ccse_ksu_logo.png" alt="CCSE KSU Logo" width="100" style="display: inline;"></a>
-		<img src="/docs/carit-logo.png" alt="Center for Applied Research in Information Technology" height="100" style="display:inline;">
-    </div>
-	<div id="header">Menu:
-		<a href="index.html">Home</a> | 
-    	<a href="about.html">About Us</a> | 
-		<a href="schedule.htm">Schedule</a> |
-        <a href="student-assistant.html">Summer Student Assistants</a>
-    </div>
+
+    <?php include 'header.php'; ?>
 
 	<div id="content">
 		<h1>Student Assistant Positions</h1>
